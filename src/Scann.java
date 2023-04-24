7683e13c3b4d0c77f6d8cfaf651a2790561d0c7b
villanueva-aurelien <aurelien.villanueva@oange.fr>
2023-04-24 19:23:44 +0200

    added change

--- src/Scann.java
+++ src/Scann.java
@@ -3,10 +3,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Comparator;
 
 public class Scann
